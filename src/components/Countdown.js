ed7daa157e0b98a2926e4d30bd1f701a2ca7b3bc
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-03-14 23:15:07 +0300

    fixing errors

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -44,7 +44,14 @@ export const Countdown = ({ minutes = 0.1, isPaused, onProgress, onEnd }) => {
     return () => clearInterval(interval.current);
   }, [isPaused]);
 
-  
+  const minute = Math.floor(millis / 1000 / 60) % 60;
+  const seconds = Math.floor(millis / 1000) % 60;
+  return (
+    <Text style={styles.text}>
+      {formatTime(minute)}:{formatTime(seconds)}
+    </Text>
+  );
+};
 
 const styles = StyleSheet.create({
   text: {
